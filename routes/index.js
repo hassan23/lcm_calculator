bd8dce50b99d56221d3730eec52c7ad76c55ac4c
hassan23 <sr.hassan23@gmail.com>
2019-07-03 22:40:51 +0530

    render to json

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var controller = require("../model/controller");
 const router = express.Router();
 
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.json({ title: "Express" });
 });
 router.post("/save", controller.save);
 router.get("/get", controller.fetchAll);
